a4c226b65c2c2edea0ece8bdf059270b960cc122
Michael Benayoun <michael@huggingface.co>
2021-09-13 18:40:57 +0200

    cmake example to compile and use tokenizers in a C++ project

---
+++ bindings/cpp/cmake-example/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <tokenizers-cpp/tokenizer.h>
+
+using namespace huggingface::tokenizers;
+
+void test_tokenizer(Tokenizer& tokenizer) {
+    auto encodings =  tokenizer.encode("My name is Morgan", true);
+    std::cout << "Encoding: 'My name is Morgan' -> " << encodings->len() << " tokens:" << std::endl;
+    std::cout << "Tokens: ";
+    for(const auto &item: encodings.get_tokens())
+        std::cout << item << " ";
+    std::cout << std::endl;
+    std::cout << "Ids: ";
+    for(const auto &item: encodings.get_ids())
+        std::cout << item << " ";
+    std::cout << std::endl;
+}
+
+int main() {
+    std::cout << "Bert Tokenizer example:" << std::endl;
+
+    std::cout << "* By proceeding step by step:" << std::endl;
+    auto builder = WordPieceBuilder::from_file("data/vocab.txt");
+    auto tokenizer = Tokenizer(builder->build());
+    tokenizer.with_normalizer(Normalizer::bert())
+            .with_pre_tokenizer(PreTokenizer::bert())
+            .with_post_processor(
+                    PostProcessor::bert("[SEP]", 101, "[CLS]", 102));
+    test_tokenizer(tokenizer);
+
+
+    std::cout << std::endl;
+    std::cout << "* By loading from file directly:" << std::endl;
+    auto tokenizer_from_file = Tokenizer::from_file("/home/michael/projects/tokenizers/bindings/cpp/cmake-example/data/tokenizer.json");
+    test_tokenizer(tokenizer_from_file);
+
+    std::cout << std::endl;
+    std::cout << "* By loading from the Huggingface Hub:" << std::endl;
+    auto tokenizer_from_hub = Tokenizer::from_pretrained("bert-base-uncased");
+    test_tokenizer(tokenizer_from_hub);
+
+    return 0;
+}
