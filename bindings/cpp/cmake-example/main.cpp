dd35db747430fdfb9b69b775dd4d744a0481556b
Michael Benayoun <mickbenayoun@gmail.com>
2021-10-27 10:55:23 +0200

    Added example for question answering

--- bindings/cpp/cmake-example/main.cpp
+++ bindings/cpp/cmake-example/main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <string>
+#include <vector>
 #include <tokenizers-cpp/tokenizer.h>
+#include <tokenizers-cpp/input_sequence.h>
 
 using namespace huggingface::tokenizers;
 
 void test_tokenizer(Tokenizer& tokenizer) {
     auto encodings =  tokenizer.encode("HuggingFace is great!", true);
-    std::cout << "Encoding: 'HuggingFace is great!' -> " << encodings->len() << " tokens:" << std::endl;
+    std::cout << "Encoding: 'Hugging Face is great!' -> " << encodings->len() << " tokens:" << std::endl;
     std::cout << "Tokens: ";
     for(const auto &item: encodings.get_tokens())
         std::cout << item << " ";
@@ -46,10 +49,37 @@ int main() {
 
     /*
     std::cout << std::endl;
-    std::cout << "* By loading from the Huggingface Hub:" << std::endl;
+    std::cout << "* By loading from the Hugging Face Hub:" << std::endl;
     auto tokenizer_from_hub = Tokenizer::from_pretrained("bert-base-uncased");
     test_tokenizer(tokenizer_from_hub);
     */
+
+    std::cout << std::endl;
+    std::cout << "* For Question Answering:" << std::endl;
+    auto question_and_context = InputSequencePair {"Who am I?", "I am Michael from Hugging Face!"};
+
+    auto max_length = 384;
+    auto pad_id = 0;
+
+    auto truncation_params = TruncationParams {};
+    truncation_params.with_strategy(TruncationStrategy::LongestFirst).with_max_length(max_length);
+
+    auto padding_params = PaddingParams {};
+    padding_params.with_pad_id(pad_id)
+        .with_fixed_length(max_length);
+
+    tokenizer_from_string.with_truncation(truncation_params);
+    tokenizer_from_string.with_padding(padding_params);
+
+    auto input_encodings = tokenizer_from_string.encode_pair(question_and_context, true);
+    std::cout << "Tokens: ";
+    for(const auto &item: input_encodings.get_tokens())
+        std::cout << item << " ";
+    std::cout << std::endl;
+    std::cout << "Ids: ";
+    for(const auto &item: input_encodings.get_ids())
+        std::cout << item << " ";
+    std::cout << std::endl;
 
     return 0;
 }
